93868111fab44b404b42de082983a22f8afe80e3
Diar Selimi <diarselimi92@gmail.com>
2016-09-03 18:34:14 +0200

    Added the seconds and some animation in the view for the seconds,

--- Frontend-projects/pomodoro/js/main.js
+++ Frontend-projects/pomodoro/js/main.js
@@ -1,5 +1,5 @@
 var minutes = 5; //default value
-var seconds = 0; //default value
+var seconds = 60; //default value
 var sessions = 4; //default value
 var sessDone = 0;
 var running = false;
@@ -20,8 +20,10 @@ var primaryButtons = $('.primary-button');
 var sessionButtons = $('.session-btn');
 var startBtn = $('.start-button');
 
+var rangerInput = $('#ranger');
 
-$(document).ready(function() {
+
+$(document).ready(function () {
     renderValues();
     renderSessions();
 
@@ -30,28 +32,28 @@ $(document).ready(function() {
     var sessBtn = $('.session-btn');
     var ranger = $('#ranger');
 
-    ranger.on('mouseup', function() {
+    ranger.on('mouseup', function () {
         var tempInput = parseInt($(this).val());
 
         if ((minutes + tempInput) < 60) {
             minutes = minutes + parseInt($(this).val());
-            if(minutes <= 0){
+            if (minutes <= 0) {
                 minutes = 1;
             }
             duration = moment.duration(minutes, 'minutes');
             renderValues();
         }
         ranger.val(0);
-        document.getElementById('ranger').value = 0;
     });
 
-    increase.on('click', function() {
+    increase.on('click', function () {
         //do the same but give him a hint
+        minutes++;
         duration = duration.add(1, 'minutes');
         renderValues();
     });
-    decrease.on('click', function() {
-        if (duration.minutes() > 0) {
+    decrease.on('click', function () {
+        if (duration.minutes() > 1) {
             duration = duration.subtract(1, 'minutes');
             minutes--;
             renderValues();
@@ -62,7 +64,7 @@ $(document).ready(function() {
             }, 200);
         }
     });
-    sessBtn.on('click', function() {
+    sessBtn.on('click', function () {
         if ($(this).data('value') == 'increase') {
             if (sessions + 1 < 10) {
                 sessions++;
@@ -80,6 +82,25 @@ $(document).ready(function() {
 function renderValues() {
     $('.minutes').html(duration.minutes()); //set the value to the html
     $('.seconds').html(duration.seconds()); //set the value to the html
+
+    var x = 50;
+    var y = 0;
+
+
+    x = 110 - 125 * Math.sin(-2 * (Math.PI) * duration.seconds() / 60);
+    y = 110 - 125 * Math.cos(-2 * (Math.PI) * duration.seconds() / 60);
+
+    //reposition the seconds
+    $('.seconds').animate({
+        'top': (y) + 'px',
+        'left': (x)  + 'px'
+    }, 200);
+
+    if (duration.minutes() == 0) {
+        Notificon(duration.seconds());
+    } else {
+        Notificon(duration.minutes());
+    }
 }
 
 /**
@@ -90,6 +111,8 @@ function renderSessions() {
     var sessionTodo = '<li class="ion-ios-circle-outline"></li>';
     var sessionDone = '<li class="ion-ios-checkmark"></li>';
     var sessionCurrent = '<li class="ion-ios-circle-filled"></li>';
+    var breakActive = '<li class="secondary-color session-dot"><i class="ion-record"></i></li>';
+    var breakDone = '<li class="ion-ios-circle-checkmark break-session"></li>';
     var dots = $('.dots');
 
     dots.html('');
@@ -110,6 +133,10 @@ function renderSessions() {
             } else {
                 dots.append(sessionTodo);
             }
+        }
+
+        if (i + 1 < sessions) {
+            dots.append(breakActive);
         }
     }
 }
@@ -135,7 +162,7 @@ function resume() {
  */
 function update(duration, intervalSpeed) {
     duration = duration.subtract(intervalSpeed, 'milliseconds');
-    if (duration.seconds() <= 0) {
+    if (duration.minutes() <= 0 && duration.seconds() <= 0) {
         sessionEnd();
     }
     renderValues();
@@ -160,7 +187,7 @@ function updateBreak(duration, intervalSpeed) {
  * @return {void}
  */
 var intervalSpeed = 1000;
-setInterval(function() {
+setInterval(function () {
     if (running) {
         update(duration, intervalSpeed);
     } else if (onBreak) {
@@ -168,7 +195,7 @@ setInterval(function() {
     } else {
         // nextSession();
     }
-}, 100);
+}, intervalSpeed);
 
 function sessionEnd() {
     renderValues();
@@ -193,11 +220,13 @@ function endBreak() {
 function showButtons() {
     primaryButtons.fadeIn('fast');
     sessionButtons.fadeIn('fast');
+    rangerInput.fadeOut('fast');
     startBtn.fadeIn('fast');
 }
 
 function hideButtons() {
     primaryButtons.fadeOut('fast');
     sessionButtons.fadeOut('fast');
+    rangerInput.fadeOut('fast');
     startBtn.fadeOut('fast');
 }
